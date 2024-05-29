b3ef3894e2a31d729252c7e4001200625805b0c9
David Barlia <barliesque@gmail.com>
2024-05-29 16:36:03 -0700

    Fixed HelpBox attribute drawer

--- Editor/HelpBoxAttributeDrawer.cs
+++ Editor/HelpBoxAttributeDrawer.cs
@@ -7,8 +7,9 @@ namespace Barliesque.InspectorTools.Editor
 	public class HelpBoxAttributeDrawer : DecoratorDrawer
 	{
 		static private GUIStyle _style;
+		private float _height = HelpBoxAttribute.enabled ? 56 : 0;
 
-		static GUIStyle HelpBoxStyle
+		static private GUIStyle HelpBoxStyle
 		{
 			get
 			{
@@ -19,21 +20,30 @@ namespace Barliesque.InspectorTools.Editor
 			}
 		}
 
-		override public float GetHeight()
+		override public float GetHeight() => _height;
+
+		private void CalculateHeight()
 		{
-			if (!HelpBoxAttribute.enabled) return 0f;
-			if (GUI.skin == null) return base.GetHeight();
-
+			if (!HelpBoxAttribute.enabled)
+			{
+				_height = 0f;
+				return;
+			}
 			var helpBoxAttribute = attribute as HelpBoxAttribute;
-			if (helpBoxAttribute == null) return base.GetHeight();
+			if (helpBoxAttribute == null || GUI.skin == null)
+			{
+				_height = base.GetHeight();
+				return;
+			}
 
-			var height = HelpBoxStyle.CalcHeight(new GUIContent(string.Format("{0}----", helpBoxAttribute.text)), EditorGUIUtility.currentViewWidth) +
+			var height = HelpBoxStyle.CalcHeight(new GUIContent($"{helpBoxAttribute.text}----"), EditorGUIUtility.currentViewWidth) +
 			             helpBoxAttribute.spaceAbove + helpBoxAttribute.spaceBelow;
-			return (helpBoxAttribute.messageType == HelpBoxType.None) ? height : Mathf.Max(height, 55f);
+			_height = (helpBoxAttribute.messageType == HelpBoxType.None) ? height : Mathf.Max(height, 55f);
 		}
 
 		override public void OnGUI(Rect position)
 		{
+			CalculateHeight();
 			if (!HelpBoxAttribute.enabled) return;
 			var helpBoxAttribute = attribute as HelpBoxAttribute;
 			if (helpBoxAttribute == null) return;
